5d62abf4075430bd1b95e4498b81511ba7c83b96
Frank Stewart <frankjstew@gmail.com>
2019-11-27 17:07:16 -0800

    Implemented Nearest Neighbour and Range Search methods nearest() and range().

--- KdTree/KdTree.java
+++ KdTree/KdTree.java
@@ -3,6 +3,7 @@ import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class KdTree {
 
@@ -44,7 +45,9 @@ public class KdTree {
 
    // add the point to the set (if it is not already in the set)
    public void insert(Point2D p) {
-      // trying way in slides
+      if (p == null) {
+         throw new IllegalArgumentException();
+      }
       this.root = insert(this.root, p, true, 0, 5, 0, 5);
 
 
@@ -52,11 +55,13 @@ public class KdTree {
    
    private Node insert(Node x, Point2D p, boolean vertical, double xMin, double xMax, double yMin, double yMax) {
       if (x == null) {
-         // if vertical, change xMax, horiz change yMax pretty sure
+         // need rectangles to be one step back, i think just dont change in this conditional
          if (vertical) {
-            return new Node(p, vertical, xMin, p.x(), yMin, yMax);
+            this.size++;
+            return new Node(p, vertical, xMin, xMax, yMin, yMax);
          } else {
-         return new Node(p, vertical, xMin, xMax, yMin, p.y());
+            this.size++;
+            return new Node(p, vertical, xMin, xMax, yMin, yMax);
          }
       }
       if (x.point.compareTo(p) == 0) {
@@ -82,12 +87,12 @@ public class KdTree {
       return x;
    }
    
-   public void printRoot(){
-      System.out.println(this.root.RST.LST.LST.point);
-      //System.out.println(this.root.vert);
-   }
+   
    // does the set contain point p? 
    public boolean contains(Point2D p) {
+      if (p == null) {
+         throw new IllegalArgumentException();
+      }
       Node x = this.root;
 
       while (x != null) {
@@ -119,7 +124,7 @@ public class KdTree {
    }            
 
    // draw all points to standard draw 
-   public void draw() { // looks like it worked first try holy fuk
+   public void draw() { // need to fix for new rectngles for range and nn
 
       StdDraw.setPenRadius(0.01);
       Queue<Node> q = new Queue<Node>();
@@ -138,12 +143,11 @@ public class KdTree {
 
          if (n.vert) {
             StdDraw.setPenColor(r);
-            StdDraw.line(n.rect.xmax(),n.rect.ymin(),n.rect.xmax(),n.rect.ymax()); //put rects in nodes to help wiht draw
-            // not as complicated as i thought, only 2^k possible lines, need to keep track of a lot of rects
+            StdDraw.line(n.point.x(),n.rect.ymin(),n.point.x(),n.rect.ymax());
          }
          if (!n.vert) {
             StdDraw.setPenColor(b);
-            StdDraw.line(n.rect.xmin(),n.rect.ymax(),n.rect.xmax(),n.rect.ymax());
+            StdDraw.line(n.rect.xmin(),n.point.y(),n.rect.xmax(),n.point.y());
          }
 
          StdDraw.setPenRadius(0.01);
@@ -159,38 +163,64 @@ public class KdTree {
 
    
    
-   //public Iterable<Point2D> range(RectHV rect)  
+   public Iterable<Point2D> range(RectHV rect) {
+      if (rect == null) {
+         throw new IllegalArgumentException();
+      }
+      Queue<Node> q = new Queue<Node>();
+      ArrayList<Point2D> pointsInRange = new ArrayList<Point2D>();
+      q.enqueue(this.root);
+      while (q.size() > 0) {
+         Node n = q.dequeue();
+         if (rect.contains(n.point)) {
+            //System.out.println(n.point);
+            //System.out.println(rect);
+            pointsInRange.add(n.point);
+         }
+         if (n.LST != null && n.LST.rect.intersects(rect)) {
+            q.enqueue(n.LST);
+         }
+         if (n.RST != null && n.RST.rect.intersects(rect)) {
+            q.enqueue(n.RST);
+         }
+         
+      }
+      return pointsInRange;
+
+   }
    // modify queue in draw should work for both           
    // all points that are inside the rectangle (or on the boundary) 
-   //public Point2D nearest(Point2D p)             
-   // a nearest neighbor in the set to point p; null if the set is empty 
-
+   public Point2D nearest(Point2D p) { //maye depth first would be faster
+      if (p == null) {
+         throw new IllegalArgumentException();
+      }
+      Queue<Node> q = new Queue<Node>();
+      double minDist = p.distanceSquaredTo(this.root.point);
+      Point2D nearest = this.root.point;
+      q.enqueue(this.root);
+      
+      while (q.size() > 0) {
+         Node n = q.dequeue();
+         //System.out.println(n.point);
+         double dist = p.distanceSquaredTo(n.point);
+         if (dist <= minDist) {
+            //System.out.print(n.point);
+            //System.out.println(dist);
+            minDist = dist;
+            nearest = n.point;
+         }
+         if (n.LST != null && n.LST.rect.distanceSquaredTo(p) < minDist) {
+            q.enqueue(n.LST);
+         }
+         if (n.RST != null && n.RST.rect.distanceSquaredTo(p) < minDist) {
+            q.enqueue(n.RST);
+         }
+      }
+      return nearest;
+   }
+   //check the child rect not parent rect for pruning
+   // a nearest neighbor in the set to point p; null if the set is empty
 
    public static void main(String[] args) {
-      Point2D p1 = new Point2D(3,2.1);
-      Point2D p2 = new Point2D(0,2.5);
-      Point2D p3 = new Point2D(1.5,3);
-      Point2D p4 = new Point2D(1.69,1);
-      Point2D p5 = new Point2D(2,2.2);
-      Point2D p6 = new Point2D(3.9,2);
-      Point2D p7 = new Point2D(1.4,1.4);
-      KdTree k = new KdTree();
-      k.insert(p1);
-      k.insert(p2);
-      k.insert(p3);
-      k.insert(p4);
-      k.insert(p5);
-      k.insert(p6);
-      k.insert(p7);
-
-      //k.printRoot();
-      //System.out.println(k.contains(p6));
-      //System.out.println(p1.compareTo(p1));
-      StdDraw.setXscale(0, 5);
-      StdDraw.setYscale(0, 5);
-      k.draw();
-
-
-   }             
-   // unit testing of the methods (optional) 
+   }              
 }
